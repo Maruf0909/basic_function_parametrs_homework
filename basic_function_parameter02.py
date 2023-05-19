ae8318d0d6700b68f76d4638a2bf0a8b99c8bab0
Mac <mac@MacBook-Air-Mac.local>
2023-05-19 17:46:55 +0500

    function02

--- basic_function_parameter02.py
+++ basic_function_parameter02.py
@@ -1,3 +1,7 @@
 # Create a function named calculate_sum that takes a list of numbers as a parameter.
 # Inside the function, calculate the sum of all the numbers in the given list.
 # Return the sum.
+def calculate_sum(a, b, c):
+    sum = a+b+c
+    return sum
+print(calculate_sum(10, 5, 3))
