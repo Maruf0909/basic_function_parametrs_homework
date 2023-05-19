84ab9a20a0963f4694c3548dffe923954ccbff20
Mac <mac@MacBook-Air-Mac.local>
2023-05-19 17:59:12 +0500

    function03

--- basic_function_parameter03.py
+++ basic_function_parameter03.py
@@ -1,3 +1,14 @@
 # Create a function named find_smallest that takes a list of numbers as a parameter.
 # Inside the function, find the smallest number in the given list.
 # Return the smallest number.
+def find_smallest(x):
+    print(x)
+    i = 0
+    n = x[0]
+    while i<len(x):
+        if x[i]<n:
+            n = x[i]
+        i = i + 1
+    return n
+
+print(find_smallest([10,20,3,94]))
