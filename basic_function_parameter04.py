76f5ce40c6521be8df2e42bbaa6301f1f6edab54
Mac <mac@MacBook-Air-Mac.local>
2023-05-19 18:29:37 +0500

    function04

--- basic_function_parameter04.py
+++ basic_function_parameter04.py
@@ -2,3 +2,9 @@
 # Inside the function, calculate the average of all the numbers in the given list.
 # Return the average.
 # Return the average.
+def calculate_average(lst):
+    s=0
+    for j in lst:
+        s+=j
+    return s/len(lst)
+print(calculate_average([1,2,3,4,5,8]))
