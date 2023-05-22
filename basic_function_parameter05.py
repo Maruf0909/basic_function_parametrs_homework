5008725ee38e98be7bb97ceb61c2ac3ec6859044
Mac <mac@MacBook-Air-Mac.local>
2023-05-22 18:22:39 +0500

    function05

--- basic_function_parameter05.py
+++ basic_function_parameter05.py
@@ -1,3 +1,13 @@
 # Create a function named count_vowels that takes a string as a parameter.
 # Inside the function, count the number of vowels (a, e, i, o, u, A, E, I, O, U) in the given string.
 # Return the count of vowels.
+def count_vowels(s):
+    count = 0
+    i = 0
+    while i<len(s):
+        if s[i] in 'aeiouAEIOU':
+            count +=1
+        i +=1
+    return count
+asdfa =count_vowels('aqwer')  
+print(asdfa)   
